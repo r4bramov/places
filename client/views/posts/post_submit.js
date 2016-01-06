c7d4d62737ea0ad1ce00251edb270c5e72b5259c
bodzie <bohdan@soften-it.com>
2016-01-06 16:11:33 +0200

    modified validation

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -3,24 +3,42 @@ Template.postSubmit.events({
         event.preventDefault();
         var file = $('.myFileInput')[0].files[0];
 
-            Images.insert(file, function (err, fileObj) {
-                if (err){
-                    console.log(err);
-                } else {
-                    var post = {
-                        photo: fileObj._id,
-                        author: $(event.target).find('[name=author]').val(),
-                        place: $(event.target).find('[name=place]').val()
-                    };
+        console.log(file);
 
-                    Meteor.call('postInsert', post, function(error, result) {
-                        if (error)
-                            return alert(error.reason);
-                        Router.go('postsList', {_id: result._id});
-                    });
 
-                }
-            });
+        var post = {
+            author: $(event.target).find('[name=author]').val(),
+            place: $(event.target).find('[name=place]').val()
+        };
+
+        Meteor.call('postInsert', post, function(error, result) {
+            if (error)
+                return console.log(error.reason);
+
+            Router.go('postsList', {_id: result._id});
+        });
+
+        Images.insert(file, function (err, fileObj) {
+            if (err){
+                console.log(err);
+            } else {
+                post = _.extend({
+                    photo: fileObj._id
+                });
+                console.log(post);
+            }
+        });
+
+            //Images.insert(file, function (err, fileObj) {
+            //    if (err){
+            //        console.log(err);
+            //    } else {
+            //        post = _.extend({
+            //            photo: fileObj._id
+            //        });
+            //        console.log(post);
+            //    }
+            //});
         //Router.go('postsList');
     }
 });
