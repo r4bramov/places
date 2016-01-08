7c99a179d720793b3bd49696ab34ec56c62e692f
bodzie <bohdan@soften-it.com>
2016-01-08 17:41:13 +0200

    modified validation

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -3,42 +3,28 @@ Template.postSubmit.events({
         event.preventDefault();
         var file = $('.myFileInput')[0].files[0];
 
-        console.log(file);
-
 
         var post = {
+            photo: '',
             author: $(event.target).find('[name=author]').val(),
             place: $(event.target).find('[name=place]').val()
         };
 
-        Meteor.call('postInsert', post, function(error, result) {
-            if (error)
-                return console.log(error.reason);
-
-            Router.go('postsList', {_id: result._id});
-        });
-
         Images.insert(file, function (err, fileObj) {
             if (err){
                 console.log(err);
-            } else {
-                post = _.extend({
-                    photo: fileObj._id
-                });
-                console.log(post);
+                return false;
             }
-        });
+            _.extend(post, {
+                photo: fileObj._id
+            });
 
-            //Images.insert(file, function (err, fileObj) {
-            //    if (err){
-            //        console.log(err);
-            //    } else {
-            //        post = _.extend({
-            //            photo: fileObj._id
-            //        });
-            //        console.log(post);
-            //    }
-            //});
-        //Router.go('postsList');
+            Meteor.call('postInsert', post, function(error, result) {
+                if (error)
+                    return console.log(error.reason);
+
+                Router.go('postsList', {_id: result._id});
+            });
+        });
     }
 });
